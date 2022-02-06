94e4d4c46a360a9d496bfcd297e86e5a4a5d2d27
Matheus Andrade <84635540+MatheusAndrade23@users.noreply.github.com>
2022-02-06 20:36:57 -0300

    Delete config directory

--- config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'a5e17935bc63556dfd943e9159e8a1aa'),
-    },
-  },
-});
