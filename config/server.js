8113fd73a5db0898dbdaa0966bcd43df6b3e9c70
MatheusAndrade23 <matheusandrade.ma2003@gmail.com>
2022-02-09 20:07:24 -0300

    Cloudnary config

--- config/server.js
+++ config/server.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
   port: env.int('PORT', 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'a5e17935bc63556dfd943e9159e8a1aa'),
+      secret: env('ADMIN_JWT_SECRET'),
     },
   },
 });
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '989959c6-64b6-4e22-b83a-7bf62cb31de7'
+};
