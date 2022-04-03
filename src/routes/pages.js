588d8559e635a557daf77634cd329538abcaa1fd
MatheusAndrade23 <matheusandrade.ma2003@gmail.com>
2022-04-03 17:41:51 -0300

    pages

--- src/routes/pages.js
+++ src/routes/pages.js
@@ -3,9 +3,12 @@ const router = express.Router();
 
 const LandingPagesController = require('../controllers/LandingPagesController');
 const PagesController = require('../controllers/PagesController');
+const LoginApiController = require('../controllers/LoginApiController');
 
 router.get('/landing-pages', LandingPagesController.ShowLandingPage);
+router.post('/login', LoginApiController.Login);
 router.post('/', PagesController.Login);
 router.get('/', PagesController.showHome);
+router.get('/:strange-slug', PagesController.notFound);
 
 module.exports = router;
