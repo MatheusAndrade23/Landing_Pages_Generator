fc9813ebc84527b7baf6f0d2fb939191e3ee2f5c
MatheusAndrade23 <matheusandrade.ma2003@gmail.com>
2022-04-09 11:38:21 -0300

    Default Routes & Controller

--- src/controllers/DefaultController.js
+++ src/controllers/DefaultController.js
@@ -2,23 +2,71 @@ const mongoose = require('mongoose');
 const User = require('../models/user');
 
 module.exports = class DefaultController {
-  static ShowFormRegister(req, res) {
-    res.render('pages/register');
-  }
-
-  static Register(req, res) {}
-
-  static ShowFormLogin(req, res) {
-    res.render('pages/login');
-  }
-
-  static Login(req, res) {}
-
   static ShowHome(req, res) {
-    if (!req.session.login) {
-      res.redirect('/login');
-    } else {
+    if (req.session.login) {
       res.render('pages/home');
+    } else {
+      res.redirect('/login');
+    }
+  }
+
+  static Login(req, res) {
+    if (req.method === 'GET') {
+      if (req.session.login) {
+        res.redirect('/');
+      } else {
+        res.status(406).render('pages/login');
+      }
+    } else if (req.method === 'POST') {
+      if (!req.body.user || !req.body.password) {
+        res.status(406).render('pages/login');
+      } else {
+        User.findOne({ user: req.body.user }, (error, user) => {
+          if (error) {
+            res.status(500).render('pages/login');
+          } else if (user) {
+            if (req.body.password === user.password) {
+              req.session.login = req.body.user;
+              res.status(200).redirect('/');
+            } else {
+              res.status(401).render('pages/login');
+            }
+          } else {
+            res.status(406).render('pages/login');
+          }
+        });
+      }
+    }
+  }
+
+  static Register(req, res) {
+    if (req.method === 'GET') {
+      res.render('pages/register');
+    } else if (req.method === 'POST') {
+      if (!req.body.user || !req.body.password) {
+        res.redirect('/register');
+      } else {
+        User.findOne({ user: req.body.user }, (error, user) => {
+          if (error) {
+            res.status(500).render('pages/register');
+          } else if (user) {
+            res.status(406).render('pages/register');
+          } else {
+            let NewUser = {
+              user: req.body.user,
+              password: req.body.password,
+              pageSelected: 3,
+            };
+            try {
+              User.crate(NewUser);
+              req.session.login = req.body.user;
+              res.status(201).redirect('/');
+            } catch (error) {
+              res.status(500).redirect('pages/register');
+            }
+          }
+        });
+      }
     }
   }
 
--- src/routes/defaultRoutes.js
+++ src/routes/defaultRoutes.js
@@ -4,14 +4,14 @@ const router = express.Router();
 const DefaultController = require('../controllers/DefaultController');
 
 router.post('/register', DefaultController.Register);
-router.get('/register', DefaultController.ShowFormRegister);
+router.get('/register', DefaultController.Register);
 
 router.post('/login', DefaultController.Login);
-router.get('/login', DefaultController.ShowFormLogin);
+router.get('/login', DefaultController.Login);
+
+router.get('/:strange', DefaultController.NotFound);
 
 router.post('/', DefaultController.Login);
 router.get('/', DefaultController.ShowHome);
-
-router.get('/:strange-slug', DefaultController.NotFound);
 
 module.exports = router;
