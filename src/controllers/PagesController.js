9cec61e6f8f0d2c2d7e3e71982e419016c267ce1
MatheusAndrade23 <matheusandrade.ma2003@gmail.com>
2022-04-08 21:55:15 -0300

    Js

--- src/controllers/PagesController.js
+++ src/controllers/PagesController.js
@@ -2,16 +2,27 @@ const mongoose = require('mongoose');
 const User = require('../models/user');
 
 module.exports = class PagesController {
+  static ShowFormRegister(req, res) {
+    res.render('pages/register');
+  }
+
+  static Register(req, res) {}
+
+  static ShowFormLogin(req, res) {
+    res.render('pages/login');
+  }
+
   static Login(req, res) {}
 
   static showHome(req, res) {
     if (!req.session.login) {
-      res.render('pages/home');
+      res.redirect('/login');
     } else {
+      res.render('pages/home');
     }
   }
 
   static notFound(req, res) {
-    res.redirect('/');
+    res.render('pages/notFound');
   }
 };
