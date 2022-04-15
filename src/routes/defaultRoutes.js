2fd54abd909be465b7a1460e0854374bc23259e8
MatheusAndrade23 <matheusandrade.ma2003@gmail.com>
2022-04-15 20:21:50 -0300

    Update defaultRoutes.js

--- src/routes/defaultRoutes.js
+++ src/routes/defaultRoutes.js
@@ -5,7 +5,7 @@ const LoginApiController = require('../controllers/LoginApiController');
 const DefaultController = require('../controllers/DefaultController');
 const ApiController = require('../controllers/ApiController');
 
-router.get('/landing-pages', ApiController.LandingPages);
+router.post('/landing-pages', ApiController.LandingPages);
 
 router.post('/register-api', LoginApiController.Register);
 router.post('/login-api', LoginApiController.Login);
@@ -15,6 +15,8 @@ router.get('/register', DefaultController.Register);
 
 router.post('/login', DefaultController.Login);
 router.get('/login', DefaultController.Login);
+
+router.post('/apply-page', DefaultController.ApplyPage);
 
 router.get('/:strange', DefaultController.NotFound);
 
